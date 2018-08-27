3428f088a28fea22adfb88236a7317861b62eb8a
Jonathan Chan <johnkchan252@gmail.com>
2018-08-27 04:10:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
   end
   
   def self.find_by_name(name)
-    self.all.find{|x| x.name==n}
+    self.all.find{|song| song.name==name}
   end
   
   def self.find_or_create_by_name(name)
